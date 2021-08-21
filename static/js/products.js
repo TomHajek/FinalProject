4c37a2b2e721c8cd32acfdb5cf910155290663b0
Tomas Hajek <hajcus10@seznam.cz>
2021-08-22 00:36:47 +0200

    WIP: working on buttons functionality

--- static/js/products.js
+++ static/js/products.js
@@ -1,12 +1,17 @@
 // Add to cart button
-/*
 let addToCartButtons = document.getElementsByClassName("update-cart");
 
 for (let i = 0; i < addToCartButtons.length; i++){
-    addToCartButtons[i].addEventListener("onclick", function(){
+    addToCartButtons[i].addEventListener("click", function(){
       let productId = this.dataset.product
       let action = this.dataset.action
       console.log("productId:", productId, "Action:", action)
+
+      console.log("USER:", user)
+        if (user === "AnonymousUser"){
+            console.log("Not logged in")
+        } else {
+            console.log("User is logged in")
+        }
     })
 }
-*/
